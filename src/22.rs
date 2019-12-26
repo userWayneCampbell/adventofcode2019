3b5c706d872d8661d412f1f5948456b1892571c2
wcampbell <wcampbell1995@gmail.com>
2019-12-25 21:06:59 -0500

    Refactor day 22 into struct
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

--- src/22.rs
+++ src/22.rs
@@ -1,28 +1,35 @@
-pub fn cut(num: f64, v: &Vec<usize>) -> Vec<usize> {
-    if num.is_sign_positive() {
-        let mut beginning = v[0..num.abs().trunc() as usize].to_vec();
-        let mut end = v[num.abs().trunc() as usize..].to_vec();
-        end.append( &mut beginning);
-        end
-    }
-    else {
-        println!("negative {}", num);
-        let mut beginning = v[(v.len() - num.abs().trunc() as usize)..].to_vec();
-        let mut end = v[0..(v.len() - num.abs().trunc() as usize)].to_vec();
-        beginning.append( &mut end);
-        println!("negative {:?} ", beginning);
-        beginning
-    }
+struct Deck {
+    v: Vec<usize>,
 }
 
-pub fn increment(num: usize, v: &Vec<usize>) -> Vec<usize> {
-    let mut position = 0;
-    let mut out_vector = vec![0; v.len()];
-    for n in 0..v.len() {
-        out_vector[position] = v[n];
-        position = (position + num) % v.len();
+impl Deck {
+    fn cut(&mut self, num: f64) {
+        if num.is_sign_positive() {
+            let mut beginning = self.v[0..num.abs().trunc() as usize].to_vec();
+            let mut end = self.v[num.abs().trunc() as usize..].to_vec();
+            end.append(&mut beginning);
+            self.v = end;
+            //end
+        }
+        else {
+            println!("negative {}", num);
+            let mut beginning = self.v[(self.v.len() - num.abs().trunc() as usize)..].to_vec();
+            let mut end = self.v[0..(self.v.len() - num.abs().trunc() as usize)].to_vec();
+            beginning.append(&mut end);
+            println!("negative {:?} ", beginning);
+            self.v = beginning;
+            //beginning
+        }
     }
-    out_vector
+    fn increment(&mut self, num: usize) {
+        let mut position = 0;
+        let mut out_vector = vec![0; self.v.len()];
+        for n in 0..self.v.len() {
+            out_vector[position] = self.v[n];
+            position = (position + num) % self.v.len();
+        }
+        self.v = out_vector
+    }
 }
 
 #[cfg(test)]
@@ -33,77 +40,101 @@ mod tests {
     fn test_cut_N_cards() {
         // positive
         let v_inp: Vec<usize> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut d = Deck { v: v_inp };
+
+        d.cut(3.0);
+
         let v_exp: Vec<usize> = vec![3, 4, 5, 6, 7, 8, 9, 0, 1, 2];
-        let out: Vec<usize> = cut(3.0, &v_inp);
-        assert_eq!(v_exp, out);
+        assert_eq!(v_exp, d.v);
 
         // negative
         let v_inp: Vec<usize> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut d = Deck { v: v_inp };
+
+        d.cut(-4.0);
+
         let v_exp: Vec<usize> = vec![6, 7, 8, 9, 0, 1, 2, 3, 4, 5];
-        let out: Vec<usize> = cut(-4.0, &v_inp);
-        assert_eq!(v_exp, out);
+        assert_eq!(v_exp, d.v);
     }
 
     #[test]
     fn test_increment() {
         let v_inp: Vec<usize> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let out: Vec<usize> = increment(3, &v_inp);
+        let mut d = Deck { v: v_inp };
+
+        d.increment(3);
+
         let v_exp: Vec<usize> = vec![0, 7, 4, 1, 8, 5, 2, 9, 6, 3];
-        assert_eq!(v_exp, out);
+        assert_eq!(v_exp, d.v);
     }
 
     #[test]
     fn test_new_stack() {
-        let mut v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        v_inp.reverse();
+        let v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut d = Deck { v: v_inp };
+
+        d.v.reverse();
+
         let v_exp = vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-        assert_eq!(v_inp, v_exp);
+        assert_eq!(v_exp, d.v);
     }
 
     #[test]
     fn test_one() {
         let v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let mut out: Vec<usize> = increment(7, &v_inp);
-        out.reverse();
-        out.reverse();
+        let mut d = Deck { v: v_inp };
+
+        d.increment(7);
+        d.v.reverse();
+        d.v.reverse();
+
         let v_exp = vec![0, 3, 6, 9, 2, 5, 8, 1, 4, 7];
-        assert_eq!(out, v_exp);
+        assert_eq!(v_exp, d.v);
     }
 
     #[test]
     fn test_two() {
         let v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let out: Vec<usize> = cut(6.0, &v_inp);
-        let mut out2: Vec<usize> = increment(7, &out);
-        out2.reverse();
+        let mut d = Deck { v: v_inp };
+
+        d.cut(6.0);
+        d.increment(7);
+        d.v.reverse();
+
         let v_exp = vec![3, 0, 7, 4, 1, 8, 5, 2, 9, 6];
-        assert_eq!(out2, v_exp);
+        assert_eq!(v_exp, d.v);
     }
 
     #[test]
     fn test_three() {
         let v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let out: Vec<usize> = increment(7, &v_inp);
-        let out2: Vec<usize> = increment(9, &out);
-        let out3: Vec<usize> = cut(-2.0, &out2);
+        let mut d = Deck { v: v_inp };
+
+        d.increment(7);
+        d.increment(9);
+        d.cut(-2.0);
+
         let v_exp = vec![6, 3, 0, 7, 4, 1, 8, 5, 2, 9];
-        assert_eq!(out3, v_exp);
+        assert_eq!(v_exp, d.v);
     }
 
     #[test]
     fn test_four() {
-        let mut v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        v_inp.reverse();
-        let out: Vec<usize> = cut(-2.0, &v_inp);
-        let out1: Vec<usize> = increment(7, &out);
-        let out2: Vec<usize> = cut(8.0, &out1);
-        let out3: Vec<usize> = cut(-4.0, &out2);
-        let out4: Vec<usize> = increment(7, &out3);
-        let out5: Vec<usize> = cut(3.0, &out4);
-        let out6: Vec<usize> = increment(9, &out5);
-        let out7: Vec<usize> = increment(3, &out6);
-        let out8: Vec<usize> = cut(-1.0, &out7);
+        let v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut d = Deck { v: v_inp };
+
+        d.v.reverse();
+        d.cut(-2.0);
+        d.increment(7);
+        d.cut(8.0);
+        d.cut(-4.0);
+        d.increment(7);
+        d.cut(3.0);
+        d.increment(9);
+        d.increment(3);
+        d.cut(-1.0);
+
         let v_exp = vec![9, 2, 5, 8, 1, 4, 7, 0, 3, 6];
-        assert_eq!(out8, v_exp);
+        assert_eq!(v_exp, d.v);
     }
 }
