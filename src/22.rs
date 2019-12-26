9468fedb7c5f70d5a889c1ecc788a7379c6bfcc6
wcampbell <wcampbell1995@gmail.com>
2019-12-25 20:37:36 -0500

    Add other day 22 tests
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

--- src/22.rs
+++ src/22.rs
@@ -59,4 +59,51 @@ mod tests {
         let v_exp = vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
         assert_eq!(v_inp, v_exp);
     }
+
+    #[test]
+    fn test_one() {
+        let v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut out: Vec<usize> = increment(7, &v_inp);
+        out.reverse();
+        out.reverse();
+        let v_exp = vec![0, 3, 6, 9, 2, 5, 8, 1, 4, 7];
+        assert_eq!(out, v_exp);
+    }
+
+    #[test]
+    fn test_two() {
+        let v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let out: Vec<usize> = cut(6.0, &v_inp);
+        let mut out2: Vec<usize> = increment(7, &out);
+        out2.reverse();
+        let v_exp = vec![3, 0, 7, 4, 1, 8, 5, 2, 9, 6];
+        assert_eq!(out2, v_exp);
+    }
+
+    #[test]
+    fn test_three() {
+        let v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let out: Vec<usize> = increment(7, &v_inp);
+        let out2: Vec<usize> = increment(9, &out);
+        let out3: Vec<usize> = cut(-2.0, &out2);
+        let v_exp = vec![6, 3, 0, 7, 4, 1, 8, 5, 2, 9];
+        assert_eq!(out3, v_exp);
+    }
+
+    #[test]
+    fn test_four() {
+        let mut v_inp = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        v_inp.reverse();
+        let out: Vec<usize> = cut(-2.0, &v_inp);
+        let out1: Vec<usize> = increment(7, &out);
+        let out2: Vec<usize> = cut(8.0, &out1);
+        let out3: Vec<usize> = cut(-4.0, &out2);
+        let out4: Vec<usize> = increment(7, &out3);
+        let out5: Vec<usize> = cut(3.0, &out4);
+        let out6: Vec<usize> = increment(9, &out5);
+        let out7: Vec<usize> = increment(3, &out6);
+        let out8: Vec<usize> = cut(-1.0, &out7);
+        let v_exp = vec![9, 2, 5, 8, 1, 4, 7, 0, 3, 6];
+        assert_eq!(out8, v_exp);
+    }
 }
