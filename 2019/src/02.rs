e1b0c545221bd4d4c3ea08fa73019467b8386cf9
wcampbell <wcampbell1995@gmail.com>
2020-11-28 10:28:17 -0500

    Add second part
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

--- 2019/src/02.rs
+++ 2019/src/02.rs
@@ -1,6 +1,7 @@
 use intcode::{Computer, OpCode};
 
 fn main() {
+    // 01
     let mut computer = Computer::new(&std::fs::read_to_string("data/02.in").unwrap());
 
     computer.memory[1] = 12;
@@ -9,4 +10,21 @@ fn main() {
     while computer.execute().is_ok() {}
 
     assert_eq!(computer.memory[0], 3716250);
+
+    // 02
+    loop {
+        for noun in 0..=99 {
+            for verb in 0..=99 {
+                let mut computer = Computer::new(&std::fs::read_to_string("data/02.in").unwrap());
+                computer.memory[1] = noun;
+                computer.memory[2] = verb;
+
+                while computer.execute().is_ok() {}
+                if computer.memory[0] == 19_690_720 {
+                    println!("answer: {}", 100 * noun + verb);
+                    return;
+                }
+            }
+        }
+    }
 }
