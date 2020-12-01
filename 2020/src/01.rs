18cc6fa3814623967694e1d55296bf2e11f45160
wcampbell <wcampbell1995@gmail.com>
2020-12-01 11:38:30 -0500

    2020: day 01
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

--- 2020/src/01.rs
+++ 2020/src/01.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("data/01.in").unwrap();
+    let memory: Vec<u32> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    // part 1
+    for mem in memory.clone() {
+        if let Some(num) = memory.iter().find(|&&a| a + mem == 2020) {
+            println!("{}", num * mem);
+            break;
+        }
+    }
+
+    // part 2
+    for mem1 in memory.clone() {
+        for mem2 in &memory {
+            for mem3 in &memory {
+                if mem1 + mem2 + mem3 == 2020 {
+                    println!("{}", mem1 * mem2 * mem3);
+                    return;
+                }
+            }
+        }
+    }
 }
