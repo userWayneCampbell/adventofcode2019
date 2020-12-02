310972233ef05dc5f00f07105960421e64cb822b
wcampbell <wcampbell1995@gmail.com>
2020-12-02 00:32:12 -0500

    2020: day 02
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

---
+++ 2020/src/02.rs
@@ -0,0 +1,46 @@
+fn main() {
+    let input = std::fs::read_to_string("data/02.in").unwrap();
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
+}
+
+fn p(input: &str) -> (Vec<&str>, Vec<usize>) {
+        let words: Vec<_> = input.split_ascii_whitespace().collect();
+        let lengths: Vec<_> = words[0]
+            .split('-')
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect();
+        (words, lengths)
+}
+
+fn part1(input: &str) -> usize {
+    let mut result = 0;
+    for line in input.lines() {
+        let (words, lengths) = p(line);
+
+        let name = words[1].trim_end_matches(':');
+        let passwords = words[2];
+
+        let count = passwords.matches(name).count();
+        if count >= lengths[0] && count <= lengths[1] {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+fn part2(input: &str) -> usize {
+    let mut result = 0;
+    for line in input.lines() {
+        let (words, lengths) = p(line);
+
+        let name = words[1].trim_end_matches(':').chars().next().unwrap();
+        let passwords: Vec<_> = words[2].chars().collect();
+
+        if (passwords[lengths[0] - 1] == name) != (passwords[lengths[1] - 1] == name) {
+            result += 1;
+        }
+    }
+    result
+}
